727451920c529b7b16dbe4c3dfd20eaaa3fb0319
peter <peter.mcintyre@gmail.com>
2026-09-11 13:32:30 +1000

    feat: add CustomerSupport AgentCore lab

---
+++ CustomerSupport/app/CustomerSupport/model/load.py
@@ -0,0 +1,8 @@
+import os
+
+from strands.models.bedrock import BedrockModel
+
+
+def load_model() -> BedrockModel:
+    """Get Bedrock model client using IAM credentials."""
+    return BedrockModel(model_id=os.environ["BEDROCK_MODEL_ID"])
